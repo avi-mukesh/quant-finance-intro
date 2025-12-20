5cf68c8fc5e9fa182b634926ee4f45a0ded1b14b
Avi Mukesh <avi.mukesh@singletrack.com>
2025-12-20 23:38:27 +0000

    annualised mean and returns of stocks

--- MarkowitzModel.py
+++ MarkowitzModel.py
@@ -4,9 +4,11 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import scipy.optimize as optimization
 
+# on average there are 252 trading days in a year
+NUM_TRADING_DAYS = 252
 
 # stocks we are going to handle
-tickers = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA']
+tickers = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA', 'NVDA']
 
 start_date = '2020-01-01'
 end_date = '2025-01-04'
@@ -25,14 +27,31 @@ def show_data(data):
     data.plot(figsize=(10, 6))
     plt.show()
 
-def calculate_return(data):
-    # e.g. data = {'AAPL': [90, 91, 92]}
-    # then data.shift(1) = {'AAPL': [NaN, 90, 91]}
-    # and data/data.shift(1) = {'AAPL': [NaN, 1.0111, 1.0109]}
-    log_return = np.log(data/data.shift(1))
-    print(log_return)
+def calculate_returns(data):
+    # e.g. data is {'AAPL': [1,2,3]}
+    # then data.shift(1) is {'AAPL': [NaN,1,2]}
+    # so data/data.shift(1) is {'AAPL': [NaN,2,1.5]}
+    log_returns = np.log(data/data.shift(1))
+    return log_returns[1:]
 
+def show_statistics(returns):
+    # e.g. if AAPL returns are [0.0027, 0.0023, ..., 0.0013]
+    # we work out the average return to be e.g. 0.0018
+    # and then multiply by NUM_TRADING_DAYS to annualize it to e.g. 0.0018 * 252 = 0.4536
+    print(returns.mean() * NUM_TRADING_DAYS)
+    print(returns.cov() * NUM_TRADING_DAYS)
+
+def show_mean_variance_of_portfolio(returns, weights):
+    # term by term multiplication of returns.mean() and weights
+    portfolio_expected_annual_return = np.sum(returns.mean() * weights) * NUM_TRADING_DAYS
+    portfolio_annualised_variance = np.dot(weights.T, np.dot(returns.cov(), weights)) * NUM_TRADING_DAYS
+    portfolio_annualised_volatility = np.sqrt(portfolio_annualised_variance)
+
+    print("Expected portfolio mean (return): ", portfolio_expected_annual_return)
+    print("Annualised portfolio standard deviation (volatility): ", portfolio_annualised_volatility)
 
 if __name__ == '__main__':
-    # dataset = download_data()
-    # show_data(dataset)
+    dataset = download_data()
+    log_daily_returns = calculate_returns(dataset)
+    show_statistics(log_daily_returns)
+    show_mean_variance_of_portfolio(log_daily_returns, weights=np.array([0.2, 0.2, 0.2, 0.2, 0.1, 0.1]))
