788ede8f8cca02166f5cfefba2a3e1b2d36af517
Avi Mukesh <avi.mukesh@singletrack.com>
2026-01-03 20:00:54 +0000

    git add .

--- BondPricingVasicek.py
+++ BondPricingVasicek.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 # simulating 1000 r(t) interest rate processes
 NUM_SIMULATIONS = 1000
-# 
 NUM_POINTS = 200
 
 # x = principal amount
@@ -14,16 +13,29 @@ def monte_carlo_simulation(F, r0, kappa, theta, sigma, T=1):
     result = []
 
     for _ in range(NUM_SIMULATIONS):
+        # each rates array is one realization of the short-rate path: {r_0, r_dt, r_2dt, ..., r_T} (one possible future evolution of interest rates)
         rates = [r0]
         for _ in range(NUM_POINTS):
             rates.append(rates[-1] + kappa * (theta - rates[-1]) * dt + sigma * np.sqrt(dt) * np.random.normal())
 
         result.append(rates)
 
-    simulation_data = pd.DataFrame(result)
-    return simulation_data.T
+    simulation_data = pd.DataFrame(result).T
+
+    # price at t of zero-coupon bond maturing at T is E[exp(-integral of r_s from s=t to s=T)]
+    # the expectation is taken under the risk-neutral measure
+    # we are using Monte-Carlo simulation in order to calculate this expectation
+    # using rectangles to numerically calculate integral
+    integrals_for_each_simulation = simulation_data.sum() * dt
+    discounted = np.exp(-integrals_for_each_simulation)
+    bond_price = F * np.mean(discounted)
+
+    print('Bond price based on Monte-Carlo simulation: %.2f' % bond_price)
+
+    return simulation_data
+
 
 if __name__ == '__main__':
-    interest_rate_paths = monte_carlo_simulation(1000, 0.1, 0.3, 0.12, 0.03)
+    interest_rate_paths = monte_carlo_simulation(1000, 0.1, 0.3, 0.3, 0.03)
     plt.plot(interest_rate_paths)
     plt.show()
