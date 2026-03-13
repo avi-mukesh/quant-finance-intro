7024553b89168f5113334fdd660cd95f4e9cfe41
Avi Mukesh <avi.mukesh@singletrack.com>
2026-03-13 09:27:31 +0000

    normal distribution of returns changes

--- NormalDistributionOfReturns.py
+++ NormalDistributionOfReturns.py
@@ -10,43 +10,46 @@ class StockData:
         self.ticker = ticker
         self.start_date = start_date
         self.end_date = end_date
+        
+    def plot_prices(self):
+        self.data.plot(ylabel='USD', xlabel='Date')
+        plt.show()
 
     def download_data(self):
-        ticker_data = yf.download(self.ticker, start=self.start_date, end=self.end_date, auto_adjust=False)
-        
-        data = {}
-        data[self.ticker] = ticker_data[['Adj Close']].squeeze()
-
-        self.data = pd.DataFrame(data)
-    
+        self.data = yf.download(self.ticker, start=self.start_date, end=self.end_date, auto_adjust=False)['Adj Close']
     def calculate_log_daily_returns(self):
         self.returns = np.log(self.data / self.data.shift(1))
         self.returns = self.returns[1:]
         
     def calculate_daily_returns(self):
-        self.returns = (self.data - self.data.shift(1))/self.data.shift(1)
-        self.returns = self.returns[1:]
+        self.returns = self.data.pct_change(periods=1)
+        self.returns = self.returns[1:]*100
 
     def returns_histogram(self):
-        plt.figure(figsize=(10,6))
+        plt.figure(figsize=(6,6))
 
         mean = self.returns.mean()
         variance = self.returns.var()
         sigma = np.sqrt(variance)
 
-        x = np.linspace(mean - 5 * sigma, mean + 5 * sigma, 100)
+        x = np.linspace(mean - 3 * sigma, mean + 3 * sigma, 100)
         y = norm.pdf(x, mean, sigma)
 
         print('mean: ', mean)
         print('var: ', variance)
 
-        plt.hist(self.returns, bins=80)
-        plt.plot(x, y)
+        plt.title('GOOGL Daily Returns from 2023-01-01 to 2026-01-01')
+        plt.hist(self.returns, bins=55)
+        plt.xlabel('Daily return %')
+        plt.ylabel('Frequency')
+        plt.xticks(np.linspace(-10,10,11))
+        # plt.plot(x, y)
         plt.show()
 
 
 if __name__ == '__main__':
-    stockData = StockData('MSFT', '2016-01-01', '2026-01-01')
+    stockData = StockData(['GOOGL'], '2023-01-01', '2026-01-01')
     stockData.download_data()
+    stockData.plot_prices()
     stockData.calculate_daily_returns()
     stockData.returns_histogram()
